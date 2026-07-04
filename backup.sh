f656c5d31bdc20e1914d25a3310f9459a2d645f8
Гадыльшин Дмитрий <gadylshin2001@gmail.com>
2026-07-04 21:26:51 +0500

    Добавлена передача на ftp-сервер

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 
+
 #!/bin/sh
 
-mkdir -p /tmp/archiv/backup
+mkdir -p /tmp/archiv/backup #создание директории, в которой будут храниться архивные бэкап файлы
 
 if [ $# -eq 1 ]; then
 	if [[ $1 != "/"* ]]; then
@@ -11,12 +12,14 @@ if [ $# -eq 1 ]; then
 		if [[ -d $1 ]]; then
 			echo "Путь указан на каталог."
 			#cp -r $1 /tmp/archiv/backup
-			tar -cf /tmp/archiv/backup/$(date +"%Y-%m-%d-%H-%M-%S".tar) -C $(dirname $1) $(basename $1)
+			tar -cf /tmp/archiv/backup/$(date +"%Y-%m-%d-%H-%M-%S".tar) -C $(dirname $1) $(basename $1) #создание архивного бэкап файла
 			if [[ $? -eq 0 ]]; then
 				echo "Архив создан успешно."
 			else
 				echo "Ошибка при создании архива."
 			fi
+			cp /tmp/archiv/backup/$(date +"%Y-%m-%d-%H-%M-%S".tar) /var/ftp/ #копирование архивного бэкап файла на ftp сервер
+
 		else
 			echo "Путь указан не на каталог. Пожалуйста, укажите путь до каталога, а не до файла."
 		fi
